b86115c78f1859449558c9d6c507d7b7c8573176
krish11189 <52022386+krish11189@users.noreply.github.com>
2021-11-11 00:26:52 -0600

    added files to get result

--- routes/van.js
+++ routes/van.js
@@ -1,9 +1,8 @@
 var express = require('express');
+const van_controlers= require('../controllers/van'); 
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('Van', { title: 'Van Search Results' });
-});
+router.get('/', van_controlers.van_view_all_Page);
 
 module.exports = router;
