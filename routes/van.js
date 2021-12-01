047ed3e3973947dce814bc281fc2bd54adb2ad5f
krish11189 <52022386+krish11189@users.noreply.github.com>
2021-11-30 18:16:38 -0600

    Added security to application

--- routes/van.js
+++ routes/van.js
@@ -1,25 +1,26 @@
 var express = require('express');
 const van_controlers= require('../controllers/van'); 
 var router = express.Router();
-
-/* GET home page. */
+// A little function to check if we have an authorized user and continue on 
+//or 
+// redirect to login. 
 const secured = (req, res, next) => {
-    if (req.user) {
-        return next();
+    console.log(req)
+    if (req.user){ 
+      return next(); 
     }
-    req.session.returnTo = req.originalUrl;
-    res.redirect("/login");
-}
-/* GET update costu
-/* GET costumes */
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
+/* GET home page. */
 router.get('/', van_controlers.van_view_all_Page);
 
-router.get('/detail', van_controlers.van_view_one_Page);
-/* GET create costume page */
-router.get('/create', van_controlers.van_create_Page);
-/* GET create update page */
-router.get('/update', van_controlers.van_update_Page);
-/* GET create costume page */
-router.get('/delete', van_controlers.van_delete_Page);
+router.get('/detail', van_controlers.van_view_one_Page); 
+
+router.get('/create',secured, van_controlers.van_create_Page); 
+
+router.get('/update',secured,van_controlers.van_update_Page);
+
+router.get('/delete',secured, van_controlers.van_delete_Page);
 
 module.exports = router;
